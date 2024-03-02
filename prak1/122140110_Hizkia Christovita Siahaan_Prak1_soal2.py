c6c84bcda72a9dfc659362dbe13cf5ef2cc00504
HizkiaSiahaan <145272145+HizkiaSiahaan@users.noreply.github.com>
2024-03-02 20:30:31 +0700

    Create 122140110_Hizkia Christovita Siahaan_Prak1_soal2.py

---
+++ prak1/122140110_Hizkia Christovita Siahaan_Prak1_soal2.py
@@ -0,0 +1,10 @@
+jari2 = int(input("Inputkan jari - jari : "))
+phi = 3.14
+
+if(jari2 < 0):
+    print ("Jari-jari lingkaran tidak boleh negatif")
+else:
+    Keliling = phi * jari2 * 2
+    Luas = phi * jari2 * jari2
+    print ("Luas = " + str(Luas))
+    print ("Keliling = " + str(Keliling))
